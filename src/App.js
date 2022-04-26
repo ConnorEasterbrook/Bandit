fd4ef33e686857be5509de1c043e8f13d02bbc69
Connor <connor2.easterbrook@live.uwe.ac.uk>
2022-04-26 14:40:22 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <main>
+      <header>
+        <div>
+          <h2>
+            Timedit
+          </h2>
+        </div>
+
+        <div>
+          <ul>
+            <li>Print</li>
+            <li>Download</li>
+            <li>Send</li>
+          </ul>
+        </div>
+      </header>
+    </main>
+  );
+}
+
+export default App;
