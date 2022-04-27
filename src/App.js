62aab40f5950d87014b8e152f2f6dcccbd03124b
Connor <connor2.easterbrook@live.uwe.ac.uk>
2022-04-27 13:00:04 +0100

    Input form styling

--- src/App.js
+++ src/App.js
@@ -69,140 +69,162 @@ function App()
                 paypalEmail={paypalEmail}
               />
 
-              <button class="editInfo" onClick={() => setShowInvoice(false)}>Edit information</button>
+              <div class="bottomSection">
+                <button class="editInfo" onClick={() => setShowInvoice(false)}>Edit information</button>
+              </div>
 
             </div>
           </div>
         ) : (
           <div class="previewDiv">
-            <label htmlFor="yourName">Your name:</label>
-            <input
-              type="text"
-              id="smallText"
-              placeholder="Enter your name."
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+            <div class="inputField">
+              <label class="singleRow" htmlFor="yourName">Your name:</label>
+              <input
+                type="text"
+                id="smallText"
+                placeholder="Enter your name."
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
 
-            <label htmlFor="address">Your address:</label>
-            <textarea
-              type="text"
-              id="bigText"
-              cols="1"
-              rows="5"
-              placeholder="Enter your address."
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-            />
+            <div class="inputFieldSingle">
+              <label htmlFor="address">Your address:</label>
+              <textarea
+                type="text"
+                id="bigText"
+                placeholder="Enter your address."
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
 
-            <label htmlFor="invoiceDate">Invoice send date:</label>
-            <input
-              type="date"
-              id="dateText"
-              placeholder="Enter the invoice send date."
-              value={invoiceDate}
-              onChange={(e) => setInvoiceDate(e.target.value)}
-            />
+            <div class="inputField">
+              <label htmlFor="invoiceDate">Invoice send date:</label>
+              <input
+                type="date"
+                id="dateText"
+                placeholder="Enter the invoice send date."
+                value={invoiceDate}
+                onChange={(e) => setInvoiceDate(e.target.value)}
+              />
+            </div>
 
-            <label htmlFor="invoiceNumber">Invoice number:</label>
-            <input
-              type="text"
-              id="smallText"
-              placeholder="Enter your invoice number."
-              value={invoiceNumber}
-              onChange={(e) => setInvoiceNumber(e.target.value)}
-            />
+            <div class="inputField">
+              <label htmlFor="dueDate">Invoice due date:</label>
+              <input
+                type="date"
+                id="dateText"
+                placeholder="Enter your invoice due date."
+                value={dueDate}
+                onChange={(e) => setDueDate(e.target.value)}
+              />
+            </div>
 
-            <label htmlFor="invoiceReference">Invoice reference:</label>
-            <input
-              type="text"
-              id="smallText"
-              placeholder="Enter your invoice reference."
-              value={invoiceReference}
-              onChange={(e) => setInvoiceReference(e.target.value)}
-            />
+            <div class="inputField">
+              <label htmlFor="invoiceNumber">Invoice number:</label>
+              <input
+                type="text"
+                id="smallText"
+                placeholder="Enter your invoice number."
+                value={invoiceNumber}
+                onChange={(e) => setInvoiceNumber(e.target.value)}
+              />
+            </div>
 
-            <label htmlFor="dueDate">Invoice due date:</label>
-            <input
-              type="date"
-              id="dateText"
-              placeholder="Enter your invoice due date."
-              value={dueDate}
-              onChange={(e) => setDueDate(e.target.value)}
-            />
-
-            <div class="gap"></div>
-
-            <label htmlFor="clientName">Client name:</label>
-            <input
-              type="text"
-              id="smallText"
-              placeholder="Enter your client's name."
-              value={clientName}
-              onChange={(e) => setClientName(e.target.value)}
-            />
-
-            <label htmlFor="clientAddress">Client address:</label>
-            <textarea
-              type="text"
-              id="bigText"
-              cols="1"
-              rows="5"
-              placeholder="Enter your client's address."
-              value={clientAddress}
-              onChange={(e) => setClientAddress(e.target.value)}>
-            </textarea>
+            <div class="inputField">
+              <label htmlFor="invoiceReference">Invoice reference:</label>
+              <input
+                type="text"
+                id="smallText"
+                placeholder="Enter your invoice reference."
+                value={invoiceReference}
+                onChange={(e) => setInvoiceReference(e.target.value)}
+              />
+            </div>
 
             <div class="gap"></div>
 
-            <label htmlFor="notes">Notes:</label>
-            <textarea
-              name="notes"
-              id="bigText"
-              cols="1"
-              rows="5"
-              placeholder="Enter any additional notes."
-              value={notes}
-              onChange={(e) => setNotes(e.target.value)}>
-            </textarea>
+            <div class="inputField">
+              <label htmlFor="clientName">Client name:</label>
+              <input
+                type="text"
+                id="smallText"
+                placeholder="Enter your client's name."
+                value={clientName}
+                onChange={(e) => setClientName(e.target.value)}
+              />
+            </div>
+
+            <div class="inputFieldSingle">
+              <label htmlFor="clientAddress">Client address:</label>
+              <textarea
+                type="text"
+                id="bigText"
+                placeholder="Enter your client's address."
+                value={clientAddress}
+                onChange={(e) => setClientAddress(e.target.value)}>
+              </textarea>
+            </div>
 
             <div class="gap"></div>
 
-            <label htmlFor="accountName">Bank account name:</label>
-            <input
-              type="text"
-              id="smallText"
-              placeholder="Enter your full name."
-              value={accountName}
-              onChange={(e) => setAccountName(e.target.value)}
-            />
+            <div class="inputFieldSingle">
+              <label htmlFor="notes">Notes:</label>
+              <textarea
+                name="notes"
+                id="bigText"
+                placeholder="Enter any additional notes."
+                value={notes}
+                onChange={(e) => setNotes(e.target.value)}>
+              </textarea>
+            </div>
 
-            <label htmlFor="accountNumber">Bank account number:</label>
-            <input
-              type="text"
-              id="smallText"
-              placeholder="Enter your account number."
-              value={accountNumber}
-              onChange={(e) => setAccountNumber(e.target.value)}
-            />
+            <div class="gap"></div>
 
-            <label htmlFor="sortCode">Bank sort code:</label>
-            <input
-              type="text"
-              id="smallText"
-              placeholder="Enter your sort code."
-              value={sortCode}
-              onChange={(e) => setSortCode(e.target.value)}
-            />
+            <div class="inputField">
+              <label htmlFor="accountName">Bank account name:</label>
+              <input
+                type="text"
+                id="smallText"
+                placeholder="Enter your full name."
+                value={accountName}
+                onChange={(e) => setAccountName(e.target.value)}
+              />
+            </div>
 
-            <label htmlFor="paypalEmail">Paypal email:</label>
-            <input
-              type="text"
-              id="smallText"
-              placeholder="Enter your Paypal email"
-              value={paypalEmail}
-              onChange={(e) => setPaypalEmail(e.target.value)}
-            />
+            <div class="inputField">
+              <label htmlFor="accountNumber">Bank account number:</label>
+              <input
+                type="text"
+                id="smallText"
+                placeholder="Enter your account number."
+                value={accountNumber}
+                onChange={(e) => setAccountNumber(e.target.value)}
+              />
+            </div>
+
+            <div class="inputField">
+              <label htmlFor="sortCode">Bank sort code:</label>
+              <input
+                type="text"
+                id="smallText"
+                placeholder="Enter your sort code."
+                value={sortCode}
+                onChange={(e) => setSortCode(e.target.value)}
+              />
+            </div>
+
+            <div class="inputField">
+              <label htmlFor="paypalEmail">Paypal email:</label>
+              <input
+                type="text"
+                id="smallText"
+                placeholder="Enter your Paypal email"
+                value={paypalEmail}
+                onChange={(e) => setPaypalEmail(e.target.value)}
+              />
+            </div>
 
             <button class="previewButton" onClick={() => setShowInvoice(true)}>Preview Invoice</button>
           </div>
